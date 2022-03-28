05a62de66781f04c9ef3c7468e0fbee251e45d92
MODIFYC <chsj5742@naver.com>
2022-03-28 22:17:39 +0900

    react

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ function App() {
         placeholder="Write your to do ... " />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => 
+        (<li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
